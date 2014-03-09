3c4e3a3aa383bd59a7bdcb326a291207bd3cfca1
pgalland <pierre.galland.x@gmail.com>
2014-03-09 23:07:21 +0100

    adds the s command (step) test 7 and adds global in the functions

--- learntris.py
+++ learntris.py
@@ -3,7 +3,7 @@ import sys
 
 
 
-############## Creation of the matrix ####################
+############## Initialization of the matrix and the score  ####################
 
 
 
@@ -13,15 +13,17 @@ import sys
 # column 0 is the leftmost column, column 9 the rightmost one
 
 matrix = [ ['.' for j in range(10)] for i in range(22)]
+score = 0
+numberOfClearedLines = 0
 
 
 
 
-################# Functions ##########################
+###################### Functions ##########################
 
 
 # prints the matrix state
-def printState() :
+def printMatrix() :
     
     # prints lines from the top to the bottom :
     for line in range(21,-1,-1) :
@@ -37,7 +39,7 @@ def printState() :
 # reads the represantation of the matrix (22 line of text from stdin)
 # and set the matrix according to the textual representation
 def setMatrix() :
-    
+    global matrix
     line = 21 # the first line read corresponds to the top line i.e. line number 21
     
     # read the lines
@@ -51,17 +53,76 @@ def setMatrix() :
             break
 
 
+# clear the matrix -> set all to '.'
+def clearMatrix() :
+    global matrix
+    for line in range(0,22):
+        for column in range(0,10):
+            matrix[line][column] = '.'
+
+
+# displays the score (initially score = 0)
+def displayScore() :
+    print(score)
+
+# displays the number of cleared lines (initially 0)
+def displayClearedLines() :
+    print(numberOfClearedLines)
+
+
+# returns True if the line is full (without '.') and False otherwise 
+def fullLine(line) :
+    
+    for column in range(0,10):
+        if (matrix[line][column] == '.'):
+            return False
+    
+    return True
+
+# clears the line #line in the matrix
+def clearLine(line) :
+    global matrix
+    for column in range(0,10):
+        matrix[line][column] = '.'
+
+
+# step searchs to see if there is a full line in the matrix (line with no '.')
+# if there is : clears the line / numberOfClearedLines+1 / score+100
+def step() :
+    global numberOfClearedLines, score, matrix
+    for line in range(0,22):
+        if fullLine(line) :
+            clearLine(line)
+            numberOfClearedLines = numberOfClearedLines+1
+            score = score+100
+        
+    
+
+
 
 ############# Scanning of the commands ######################
 
 
 for line in sys.stdin :
     
-    if (line == 'p\n'):
-        printState()
-    elif (line == 'g\n'):
+    if (line == 'p\n'): # print matrix
+        printMatrix()
+
+    elif (line == 'g\n'):# set matrix to given matrix
         setMatrix()
-    else :
+
+    elif (line == 'c\n'): # clear matrix
+        clearMatrix()
+
+    elif (line == '?s\n'): # query for the score
+        displayScore()
+
+    elif (line == '?n\n'): # query for the numbers of cleared lines
+        displayClearedLines()
+
+    elif (line == 's\n'): # executes one step
+        step()
+
+    else:
         quit()
-    
 
