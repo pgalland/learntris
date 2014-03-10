e149b4e67a452378139a20405d50a82af9d44355
pgalland <pierre.galland.x@gmail.com>
2014-03-10 22:28:58 +0100

    adds all the shapes

--- learntris.py
+++ learntris.py
@@ -12,24 +12,82 @@ import sys
 # line 0 is the bottom line and line 21 the top one
 # column 0 is the leftmost column, column 9 the rightmost one
 
-matrix = [ ['.' for j in range(10)] for i in range(22)]
+matrix = [ ['.' for column in range(10)] for line in range(22)]
 score = 0
 numberOfClearedLines = 0
 
 
+############ Definitions of the shapes #######################
 
+# shape I
+
+shapeI = [ ['.' for column in range(4)] for line in range(4)]
+
+for column in range(4):
+    shapeI[2][column] = 'c'
+
+
+# shape 0
+# it's the letter not the number
+shapeO = [ ['y' for column in range(2)] for line in range(2)]
+
+
+# shape Z
+
+shapeZ = [ ['.' for column in range(3)] for line in range(3)]
+shapeZ[1][1]='r'
+shapeZ[1][2]='r'
+shapeZ[2][0]='r'
+shapeZ[2][1]='r'
+
+# shape S
+
+shapeS = [ ['.' for column in range(3)] for line in range(3)]
+shapeS[1][0] ='g'
+shapeS[1][1] ='g'
+shapeS[2][1] ='g'
+shapeS[2][2] ='g'
+
+# shape J
+
+shapeJ = [ ['.' for column in range(3)] for line in range(3)]
+shapeJ[1][0] ='b'
+shapeJ[1][1] ='b'
+shapeJ[1][2] ='b'
+shapeJ[2][0] ='b'
+
+# shape L
+
+shapeL = [ ['.' for column in range(3)] for line in range(3)]
+shapeL[1][0] ='o'
+shapeL[1][1] ='o'
+shapeL[1][2] ='o'
+shapeL[2][2] ='o'
+
+# shape T
+
+shapeT = [ ['.' for column in range(3)] for line in range(3)]
+shapeT[1][0] ='m'
+shapeT[1][1] ='m'
+shapeT[1][2] ='m'
+shapeT[2][1] ='m'
 
 ###################### Functions ##########################
 
 
 # prints the matrix state
-def printMatrix() :
+def printMatrix(matrix) :
     
+    if (len(matrix) == 0):
+        return
+
+    numberOfLines = len(matrix)
+    numberOfColumns = len(matrix[0])
     # prints lines from the top to the bottom :
-    for line in range(21,-1,-1) :
+    for line in range(numberOfLines-1,-1,-1) :
         #prints columns from left to right
-        for column in range(0,10) :
-            if (column<9) :
+        for column in range(0,numberOfColumns) :
+            if (column<(numberOfColumns-1)) :
                 print(matrix[line][column], end=" ")
             else :
                 print(matrix[line][column], end="\n") #prints newline for the last column
@@ -106,7 +164,10 @@ def step() :
 for line in sys.stdin :
     
     if (line == 'p\n'): # print matrix
-        printMatrix()
+        printMatrix(matrix)
+
+    elif (line == 'q\n'):
+        quit()
 
     elif (line == 'g\n'):# set matrix to given matrix
         setMatrix()
@@ -122,7 +183,34 @@ for line in sys.stdin :
 
     elif (line == 's\n'): # executes one step
         step()
-
-    else:
+    
+    elif (line == 'I t q\n'):
+        printMatrix(shapeI)
         quit()
 
+    elif (line == 'O t q\n'):
+        printMatrix(shapeO)
+        quit()
+
+    elif (line == 'Z t q\n'):
+        printMatrix(shapeZ)
+        quit()
+    
+    elif (line == 'S t q\n'):
+        printMatrix(shapeS)
+        quit()
+
+    elif (line == 'J t q\n'):
+        printMatrix(shapeJ)
+        quit()
+
+    elif (line == 'L t q\n'):
+        printMatrix(shapeL)
+        quit()
+
+    elif (line == 'T t q\n'):
+        printMatrix(shapeT)
+        quit()
+
+    else :
+        quit()
